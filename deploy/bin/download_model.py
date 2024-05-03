56b3e0a507ae1b820678616f249183c99306eb7c
Mohamed Magdy <64127744+mohamedmagdy841@users.noreply.github.com>
2024-05-03 14:06:29 +0300

    Add files via upload

--- deploy/bin/download_model.py
+++ deploy/bin/download_model.py
@@ -1,1 +1,6 @@
+import gdown
 
+gdown.download(
+    "https://drive.google.com/file/d/1W6CA4HbzHeZ9aMixkSh6qNSirgOm6CfD/view?usp=drive_link",
+    "assets/model_0.bin",
+)
---
+++ deploy/bin/start_server.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+uvicorn sentiment_classifier.api:app
